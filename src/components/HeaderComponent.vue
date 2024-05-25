e51dfc8438182a757ccc04ed1dffa4a26e1e518a
Alexandru Kitsenco <64800754+alexandrukitsenco@users.noreply.github.com>
2024-05-25 18:53:38 +0200

    new files

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout>
+    <v-app-bar color="primary" density="compact">
+      <template v-slot:append>
+        <v-btn icon="mdi-login" class="ml-2" @click="navigateToLogin"></v-btn>
+      </template>
+    </v-app-bar>
+  </v-layout>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const navigateToLogin = () => {
+  router.push('/login')
+}
+</script>
