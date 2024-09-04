6a4bee07057fa935da079296c7bb55f60e1f47e0
Alexandru Kitsenco <64800754+alexandrukitsenco@users.noreply.github.com>
2024-09-04 18:37:27 +0200

    fix result

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,17 +1,22 @@
 <template>
   <v-container style="display: flex; flex-direction: column; gap: 20px;">
-        <v-row><p>Resultado del análisis de maduración {{ imageStore.prediction }}</p></v-row>
-        <v-row><img style="width: 100%;" v-if="imageStore.uploadedImage" :src="imageStore.uploadedImage" alt="Imagen analizada" /></v-row>
-        <v-row><v-btn @click="goBack" color="primary" style="width: 100%">Volver</v-btn></v-row>        
+    <v-row><p>Resultado del análisis de maduración {{ formattedPrediction }}</p></v-row>
+    <v-row><img style="width: 100%;" v-if="imageStore.uploadedImage" :src="imageStore.uploadedImage" alt="Imagen analizada" /></v-row>
+    <v-row><v-btn @click="goBack" color="primary" style="width: 100%">Volver</v-btn></v-row>        
   </v-container>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { useImageStore } from '@/stores/imageStore'
+import { computed } from 'vue'
 
 const router = useRouter()
 const imageStore = useImageStore()
+
+const formattedPrediction = computed(() => {
+  return Number(imageStore.prediction).toFixed(2)
+})
 
 const goBack = () => {
   router.push({ name: 'home' })
